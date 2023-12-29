f9cca70691ff1dac47e819e449e1d856d729b4be
Harsh Mohan <harshmohan813@gmail.com>
2023-12-29 20:59:51 +0000

    produce 3d sphere as pointcloud2

--- solution/solution/test_detection.py
+++ solution/solution/test_detection.py
@@ -4,25 +4,31 @@ import rclpy
 from rclpy.node import Node
 from rclpy.signals import SignalHandlerOptions
 from rclpy.executors import ExternalShutdownException
-from sensor_msgs.msg import PointCloud
+from sensor_msgs.msg import PointCloud, PointCloud2, PointField
 from geometry_msgs.msg      import Point, Point32
 from visualization_msgs.msg import Marker
 from assessment_interfaces.msg import ItemList, Item, HomeZone, ItemHolders
 import math
+from sensor_msgs_py import point_cloud2
+from std_msgs.msg import Header
 import numpy as np
+import struct
 
 class RobotController(Node):
 
     def __init__(self):
-        super().__init__('detect_ball_3d')
+        super().__init__('detect_ball_3d', namespace='robot1')
 
         self.get_logger().info('Detecting in 3D')
+        ns = self.get_namespace()
 
-        self.ball2d_sub  = self.create_subscription(ItemList,"/robot1/items",self.ball_rcv_callback, 10)
+        self.ball2d_sub  = self.create_subscription(ItemList,f"{ns}/items",self.ball_rcv_callback, 10)
         self.ball3d_pub  = self.create_publisher(Point,"/detected_ball_3d",1)
         self.ball_marker_pub  = self.create_publisher(Marker,"/ball_3d_marker",1)
-        self.ball_cloud_pub = self.create_publisher(PointCloud, "/ball_3d_cloud", 1)
+        # self.ball_cloud_pub = self.create_publisher(PointCloud, "/ball_3d_cloud", 1)
+        self.ball_cloud_pub = self.create_publisher(PointCloud2, "/ball_3d_cloud", 1)
 
+        # self.camera_frame = "robot1/camera_link"
         self.camera_frame = "camera_link"
         
         self.width = 640
@@ -39,8 +45,9 @@ class RobotController(Node):
         if len(items.data) == 0: 
             return
         
-        cloud = PointCloud()
-        cloud.header.frame_id = self.camera_frame
+        # cloud = PointCloud()
+        # cloud.header.frame_id = self.camera_frame
+        points = []
         
         for i, item in enumerate(items.data):
             u = item.x / (self.width / 2)
@@ -64,21 +71,32 @@ class RobotController(Node):
             sphere_center_x = p.x = z
             sphere_center_y = p.y = x
             sphere_center_z = p.z = 0.0
+            
+            self.get_logger().info(f"Ball 3D: {p}")
+            
             # self.ball3d_pub.publish(p)
             # cloud.points.append(p)
+            # sphere_radius = 0.075  # Adjust radius of the sphere as needed
+            # for phi in np.linspace(0, math.pi, 10):  # Adjust granularity
+            #     for theta in np.linspace(0, 2 * math.pi, 20):  # Adjust granularity
+            #         dx = sphere_radius * math.sin(phi) * math.cos(theta)
+            #         dy = sphere_radius * math.sin(phi) * math.sin(theta)
+            #         dz = sphere_radius * math.cos(phi)
+
+            #         p = Point32()
+            #         p.x, p.y, p.z = sphere_center_x + dx, sphere_center_y + dy, sphere_center_z + dz
+            #         cloud.points.append(p)
             sphere_radius = 0.075  # Adjust radius of the sphere as needed
             for phi in np.linspace(0, math.pi, 10):  # Adjust granularity
                 for theta in np.linspace(0, 2 * math.pi, 20):  # Adjust granularity
                     dx = sphere_radius * math.sin(phi) * math.cos(theta)
                     dy = sphere_radius * math.sin(phi) * math.sin(theta)
                     dz = sphere_radius * math.cos(phi)
+                    r, g, b = 255, 0, 0  # Red color
+                    rgb = struct.unpack('I', struct.pack('BBBB', b, g, r, 0))[0]  # Pack RGB into a single float value
 
-                    p = Point32()
-                    p.x, p.y, p.z = sphere_center_x + dx, sphere_center_y + dy, sphere_center_z + dz
-                    cloud.points.append(p)
-            
-            self.get_logger().info(f"Ball 3D: {p}")
-            
+                    # Append each point to the points list
+                    points.append([sphere_center_x + dx, sphere_center_y + dy, sphere_center_z + dz, rgb])
 
             # m = Marker()
             # m.header.frame_id = self.camera_frame
@@ -96,6 +114,12 @@ class RobotController(Node):
             # m.color.g = 1.0
             # m.color.b = 0.0
             # m.color.a = 1.0
+            
+            # delete_marker = Marker()
+            # delete_marker.header.frame_id = "your_frame_id"  # same frame_id as your markers
+            # delete_marker.id = i - 1  # ID of the marker to delete
+            # delete_marker.action = Marker.DELETE
+            # publisher.publish(delete_marker)
 
             # self.ball_marker_pub.publish(m)
             
@@ -104,7 +128,20 @@ class RobotController(Node):
         # data.y = float(item.y/240.0)
         # data.z = float(item.diameter)
         
+        # self.ball_cloud_pub.publish(cloud)
+        header = Header()
+        header.stamp = self.get_clock().now().to_msg()
+        header.frame_id = self.camera_frame
+
+        fields = [PointField(name='x', offset=0, datatype=PointField.FLOAT32, count=1),
+                  PointField(name='y', offset=4, datatype=PointField.FLOAT32, count=1),
+                  PointField(name='z', offset=8, datatype=PointField.FLOAT32, count=1),
+                  # Add an RGB field. The datatype here is a 32-bit float, but it will be interpreted as packed 8-bit R, G, B, and unused.
+                  PointField(name='rgb', offset=12, datatype=PointField.FLOAT32, count=1)]
+
+        cloud = point_cloud2.create_cloud(header, fields, points)
         self.ball_cloud_pub.publish(cloud)
+        # self.get_logger().info(f"Ball 3D: {cloud}")
         # print(m.pose.position)
 
 
