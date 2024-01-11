f9aec5cc3b6514bf0b7a092c93b49ec150442300
Harsh Mohan <harshmohan813@gmail.com>
2024-01-11 21:41:09 +0000

    use OpaqueFunction for launch file

--- solution/launch/sol_launch.py
+++ solution/launch/sol_launch.py
@@ -1,12 +1,77 @@
 import os
+import yaml
+
 
 from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch import LaunchDescription, LaunchContext
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, GroupAction, OpaqueFunction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
+from launch_ros.actions import Node, PushRosNamespace, SetRemap, SetParameter
 
+def robot_controller_actions(context : LaunchContext):
+
+    num_robots = int(context.launch_configurations['num_robots'])
+        
+    yaml_path = os.path.join(get_package_share_directory('assessment'), 'config', 'initial_poses.yaml')
+
+    with open(yaml_path, 'r') as f:
+        configuration = yaml.safe_load(f)
+
+    initial_poses = configuration[num_robots]
+
+    actions = []
+
+    for robot_number in range(1, num_robots + 1):
+        robot_name = 'robot' + str(robot_number)
+        
+        pos = initial_poses[robot_name]
+
+        group = GroupAction([
+
+            PushRosNamespace(robot_name),
+            
+            Node(
+                package='solution',
+                output='screen',
+                executable='sensor_fusion',
+                name='sensor_fusion',
+            ),
+            
+            Node(
+                package='solution',
+                output='screen',
+                executable='item_cloud',
+                name='item_cloud',
+            ),
+            
+            Node(
+                package='solution',
+                output='screen',
+                executable='lidar_cloud',
+                name='lidar_cloud',
+            ),
+            
+            Node(
+                package='solution',
+                output='screen',
+                executable='state_marker_transformer',
+                name='state_marker_transformer',
+                parameters=[{'r': 1.0, 'g': 0.0, 'b': 0.0}],
+            ),
+            
+            Node(
+                package='solution',
+                output='screen',
+                executable='sol',
+                name='sol',
+                parameters=[{'x_pose': pos['x'], 'y_pose': pos['y'], 'yaw': pos['yaw']}]
+            ),
+        ])
+
+        actions.append(group)
+
+    return actions
 
 def generate_launch_description():
     launch_file_dir = os.path.join(get_package_share_directory('assessment'), 'launch')
@@ -34,74 +99,28 @@ def generate_launch_description():
             }.items()
     )
     
-    x_pose = LaunchConfiguration('x_pose', default='-3.5')
-    y_pose = LaunchConfiguration('y_pose', default='0.0')
-    
     traffic_manager=Node(
         package='solution',
         output='screen',
         executable='traffic_manager',
         name='traffic_manager',
-        parameters=[{'use_sim_time': use_sim_time, 'num_robots': num_robots}],
+        parameters=[{'num_robots': num_robots}],
     )
+    
+    robot_controller_cmd = OpaqueFunction(function=robot_controller_actions)
 
-    sensor_fusion=Node(
-        package='solution',
-        output='screen',
-        executable='sensor_fusion',
-        name='sensor_fusion',
-        namespace='robot1',
-        parameters=[{'use_sim_time': use_sim_time}],
-    )
-    
-    item_cloud=Node(
-        package='solution',
-        output='screen',
-        executable='item_cloud',
-        name='item_cloud',
-        namespace='robot1',
-        parameters=[{'use_sim_time': use_sim_time}],
-    )
-    
-    lidar_cloud=Node(
-        package='solution',
-        output='screen',
-        executable='lidar_cloud',
-        name='lidar_cloud',
-        namespace='robot1',
-    )
-    
-    state_marker_transformer=Node(
-        package='solution',
-        output='screen',
-        executable='state_marker_transformer',
-        name='state_marker_transformer',
-        namespace='robot1',
-        parameters=[{'r': 1.0, 'g': 0.0, 'b': 0.0}],
-    )
-    
-    sol=Node(
-        package='solution',
-        output='screen',
-        executable='sol',
-        name='sol',
-        namespace='robot1',
-        parameters=[{'use_sim_time': use_sim_time, 'x_pose': x_pose, 'y_pose': y_pose}],
-    )
     
     ld = LaunchDescription()
+    
+    ld.add_action(SetParameter(name='use_sim_time', value=True))
+    
+    ld.add_action(traffic_manager)
     
     ld.add_action(declare_num_robots_cmd)
 
     # Add the commands to the launch description
     ld.add_action(assessment_launch)
     
-    ld.add_action(traffic_manager)
-    
-    ld.add_action(sensor_fusion)
-    ld.add_action(item_cloud)
-    ld.add_action(lidar_cloud)
-    ld.add_action(state_marker_transformer)
-    ld.add_action(sol)
+    ld.add_action(robot_controller_cmd)
     
     return ld
