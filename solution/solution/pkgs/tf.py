42cc677d570ae13b82f2f766b3731c83b3f96949
Harsh Mohan <harshmohan813@gmail.com>
2024-01-28 07:48:47 +0000

    add tf

---
+++ solution/solution/pkgs/tf.py
@@ -0,0 +1,20 @@
+import math
+
+def get_euler_from_quaternion(quaternion):
+    x = quaternion.x
+    y = quaternion.y
+    z = quaternion.z
+    w = quaternion.w
+
+    sinr_cosp = 2 * (w * x + y * z)
+    cosr_cosp = 1 - 2 * (x * x + y * y)
+    roll = math.atan2(sinr_cosp, cosr_cosp)
+
+    sinp = 2 * (w * y - z * x)
+    pitch = math.asin(sinp)
+
+    siny_cosp = 2 * (w * z + x * y)
+    cosy_cosp = 1 - 2 * (y * y + z * z)
+    yaw = math.atan2(siny_cosp, cosy_cosp)
+
+    return roll, pitch, yaw
